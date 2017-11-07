e03e2d390e97c2c92fa62272ca6408602a9f60b1
Hoan Nguyen <htonynguyen@gmail.com>
2017-11-07 17:38:07 -0500

    Game Logic

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,5 @@
 var gridSystem;
+var neighborGrid;
 var width;
 var height;
 function start() {
@@ -6,12 +7,15 @@ function start() {
     height = $('#gridHeight').val();
     console.log("Grid is "+width+" by "+height);
     gridSystem = new Array(height);
+    neighborGrid = new Array(height);
     for (i = 0; i < height; i++) {
         gridSystem[i] = new Array(width);
+        neighborGrid[i] = new Array(width);
     }
     for (i = 0; i < height; i++) {
         for (j = 0; j < width; j++) {
             gridSystem[i].push(0);
+            neighborGrid[i].push(0);
         }
     }
     $('#heading').hide('slow');
@@ -19,6 +23,7 @@ function start() {
     console.log(gridSystem);
 }
 function drawBoard() {
+    console.log("Drawing board...");
     grid = "<table id=\"potato\">";
     for (i = 0; i < height; i++) {
         grid += "<tr>";
@@ -48,16 +53,35 @@ function drawBoard() {
     
 }
 function incGen(num) {
+    console.log(num);
     for (i = 0; i < num; i++) {
         for (j = 0; j < gridSystem.length; j++) {
             for (k = 0; k < gridSystem[j].length; k++) {
                 neighbor = 0;// Check surrounding cells
                 if (j > 0) {
+                    if (k > 0) {
+                        if (gridSystem[j-1][k-1] == 1) {
+                            neighbor++;
+                        }
+                    } else if (k != width) {
+                        if (gridSystem[j-1][k+1] == 1) {
+                            neighbor++;
+                        }
+                    }
                     if (gridSystem[j-1][k] == 1) {
                         neighbor++;
                     }
                 }
                 if (j != (height-1)) {
+                    if (k > 0) {
+                        if (gridSystem[j+1][k-1] == 1) {
+                            neighbor++;
+                        }
+                    } else if (k != width) {
+                        if (gridSystem[j+1][k+1] == 1) {
+                            neighbor++;
+                        }
+                    }
                     if (gridSystem[j+1][k] == 1) {
                         neighbor++;
                     }
@@ -72,7 +96,12 @@ function incGen(num) {
                         neighbor++;
                     }
                 }
-                console.log(neighbor);
+                neighborGrid[j][k] = neighbor;
+            }
+        }
+        for (j = 0; j < gridSystem.length; j++) {
+            for (k = 0; k < gridSystem[j].length; k++) {
+                neighbor = neighborGrid[j][k];
                 if (neighbor < 2 || neighbor > 3 || (gridSystem[j][k] == 1 && neighbor > 3)) {
                     gridSystem[j][k] = 0;
                 } else if (gridSystem[j][k] == 1 && (neighbor == 2 || neighbor == 3)) {
@@ -80,8 +109,8 @@ function incGen(num) {
                 }
             }
         }
-        drawBoard();
     }
+    drawBoard();
 }
 
 function reset() {
