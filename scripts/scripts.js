f496184edc5a3876fc3fb5e34a53bc4c0fd693b7
htonyn-d <htonynguyen@gmail.com>
2017-11-07 21:22:44 -0500

    Added Patterns

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,9 @@ var gridSystem;
 var neighborGrid;
 var width;
 var height;
+var active = 0;
+var autoRun;
+var patternSelect = 0;
 function start() {
     width = $('#gridWidth').val();
     height = $('#gridHeight').val();
@@ -19,6 +22,7 @@ function start() {
         }
     }
     $('#heading').hide('slow');
+    $('#startButton').hide('slow');
     drawBoard();
     console.log(gridSystem);
 }
@@ -43,11 +47,161 @@ function drawBoard() {
         var col = $(this).parent().children().index($(this));
         var row = $(this).closest('tr').index();
         console.log("Row: "+row+"; Column: "+col);
-        if (gridSystem[row][col] == 1) {
-            gridSystem[row][col] = 0;
-        } else {
+        if (patternSelect == 0) {
+            if (gridSystem[row][col] == 1) {
+                gridSystem[row][col] = 0;
+            } else {
+                gridSystem[row][col] = 1;
+            }
+        } else if (patternSelect == 1) {
             gridSystem[row][col] = 1;
+            gridSystem[row+1][col] = 1;
+            gridSystem[row+1][col+1] = 1;
+            gridSystem[row][col+1] = 1;
+        } else if (patternSelect == 2) {
+            gridSystem[row][col+1] = 1;
+            gridSystem[row+1][col+2] = 1;
+            gridSystem[row+2][col] = 1;
+            gridSystem[row+2][col+1] = 1;
+            gridSystem[row+2][col+2] = 1;
+        } else if (patternSelect == 3) {
+            rand = Math.floor(Math.random() * 2);
+            if (rand%2 == 0) {
+                gridSystem[row][col] = 1;
+                gridSystem[row][col+1] = 1;
+                gridSystem[row][col+2] = 1;
+            } else {
+                gridSystem[row][col] = 1;
+                gridSystem[row+1][col] = 1;
+                gridSystem[row+2][col] = 1;
+            }
+        } else if (patternSelect == 4) {
+            gridSystem[row][col] = 1;
+            gridSystem[row][col+1] = 1;
+            gridSystem[row+1][col] = 1;
+            gridSystem[row+2][col+3] = 1;
+            gridSystem[row+3][col+2] = 1;
+            gridSystem[row+3][col+3] = 1;
+        } else if (patternSelect == 5) {
+            gridSystem[row][col+1] = 1;
+            gridSystem[row][col+2] = 1;
+            gridSystem[row][col+3] = 1;
+            gridSystem[row+1][col] = 1;
+            gridSystem[row+1][col+1] = 1;
+            gridSystem[row+1][col+2] = 1;
+        } else if (patternSelect == 6) {
+            gridSystem[row][col+4] = 1;
+            gridSystem[row][col+10] = 1;
+            gridSystem[row+1][col+4] = 1;
+            gridSystem[row+1][col+10] = 1;
+            gridSystem[row+2][col+4] = 1;
+            gridSystem[row+2][col+5] = 1;
+            gridSystem[row+2][col+9] = 1;
+            gridSystem[row+2][col+10] = 1;
+            gridSystem[row+4][col] = 1;
+            gridSystem[row+4][col+1] = 1;
+            gridSystem[row+4][col+2] = 1;
+            gridSystem[row+4][col+5] = 1;
+            gridSystem[row+4][col+6] = 1;
+            gridSystem[row+4][col+8] = 1;
+            gridSystem[row+4][col+9] = 1;
+            gridSystem[row+4][col+12] = 1;
+            gridSystem[row+4][col+13] = 1;
+            gridSystem[row+4][col+14] = 1;
+            gridSystem[row+5][col+2] = 1;
+            gridSystem[row+5][col+4] = 1;
+            gridSystem[row+5][col+6] = 1;
+            gridSystem[row+5][col+8] = 1;
+            gridSystem[row+5][col+10] = 1;
+            gridSystem[row+5][col+12] = 1;
+            gridSystem[row+6][col+4] = 1;
+            gridSystem[row+6][col+5] = 1;
+            gridSystem[row+6][col+9] = 1;
+            gridSystem[row+6][col+10] = 1;
+            gridSystem[row+8][col+4] = 1;
+            gridSystem[row+8][col+5] = 1;
+            gridSystem[row+8][col+9] = 1;
+            gridSystem[row+8][col+10] = 1;
+            gridSystem[row+9][col+2] = 1;
+            gridSystem[row+9][col+4] = 1;
+            gridSystem[row+9][col+6] = 1;
+            gridSystem[row+9][col+8] = 1;
+            gridSystem[row+9][col+10] = 1;
+            gridSystem[row+9][col+12] = 1;
+            gridSystem[row+10][col] = 1;
+            gridSystem[row+10][col+1] = 1;
+            gridSystem[row+10][col+2] = 1;
+            gridSystem[row+10][col+5] = 1;
+            gridSystem[row+10][col+6] = 1;
+            gridSystem[row+10][col+8] = 1;
+            gridSystem[row+10][col+9] = 1;
+            gridSystem[row+10][col+12] = 1;
+            gridSystem[row+10][col+13] = 1;
+            gridSystem[row+10][col+14] = 1;
+            gridSystem[row+12][col+4] = 1;
+            gridSystem[row+12][col+5] = 1;
+            gridSystem[row+12][col+9] = 1;
+            gridSystem[row+12][col+10] = 1;
+            gridSystem[row+13][col+4] = 1;
+            gridSystem[row+13][col+10] = 1;
+            gridSystem[row+14][col+4] = 1;
+            gridSystem[row+14][col+10] = 1;           
+            
+        } else if (patternSelect == 7) {
+            gridSystem[row][col] = 1;
+            gridSystem[row][col+3] = 1;
+            gridSystem[row+2][col] = 1;
+            gridSystem[row+3][col+1] = 1;
+            gridSystem[row+3][col+2] = 1;
+            gridSystem[row+3][col+3] = 1;
+            gridSystem[row+1][col+4] = 1;
+            gridSystem[row+2][col+4] = 1;
+            gridSystem[row+3][col+4] = 1;
+        } else if (patternSelect == 8) {
+            gridSystem[row+4][col] = 1;
+            gridSystem[row+4][col+1] = 1;
+            gridSystem[row+5][col] = 1;
+            gridSystem[row+5][col+1] = 1;
+            gridSystem[row+2][col+34] = 1;
+            gridSystem[row+2][col+35] = 1;
+            gridSystem[row+3][col+34] = 1;
+            gridSystem[row+3][col+35] = 1;
+            gridSystem[row+4][col+10] = 1;
+            gridSystem[row+5][col+10] = 1;
+            gridSystem[row+6][col+10] = 1;
+            gridSystem[row+3][col+11] = 1;
+            gridSystem[row+7][col+11] = 1;
+            gridSystem[row+2][col+12] = 1;
+            gridSystem[row+8][col+12] = 1;
+            gridSystem[row+2][col+13] = 1;
+            gridSystem[row+8][col+13] = 1;
+            gridSystem[row+5][col+14] = 1;
+            gridSystem[row+3][col+15] = 1;
+            gridSystem[row+7][col+15] = 1;
+            gridSystem[row+4][col+16] = 1;
+            gridSystem[row+5][col+16] = 1;
+            gridSystem[row+6][col+16] = 1;
+            gridSystem[row+5][col+17] = 1;
+            gridSystem[row+2][col+20] = 1;
+            gridSystem[row+3][col+20] = 1;
+            gridSystem[row+4][col+20] = 1;
+            gridSystem[row+2][col+21] = 1;
+            gridSystem[row+3][col+21] = 1;
+            gridSystem[row+4][col+21] = 1;
+            gridSystem[row+1][col+22] = 1;
+            gridSystem[row+5][col+22] = 1;
+            gridSystem[row][col+24] = 1;
+            gridSystem[row+1][col+24] = 1;
+            gridSystem[row+5][col+24] = 1;
+            gridSystem[row+6][col+24] = 1;           
+        } else {
+            if (gridSystem[row][col] == 1) {
+                gridSystem[row][col] = 0;
+            } else {
+                gridSystem[row][col] = 1;
+            }
         }
+        
         drawBoard();
     });
     
@@ -139,4 +293,126 @@ function reset() {
         }
     }
     drawBoard();
+}
+
+function realTime() {
+    if (active) {
+        active = 0;
+        $('#realtime').html('Run');
+        clearInterval(autoRun);
+    } else {
+        active = 1;
+        $('#realtime').html('Stop');
+        autoRun = setInterval(function() {
+            console.log("Iterating");
+            for (j = 0; j < gridSystem.length; j++) {
+                // Per cell in the row
+                for (k = 0; k < gridSystem[j].length; k++) {
+                    neighbor = 0; // Reset neighbor count
+                    // Checks if the cell is NOT at the top row.
+                    if (j > 0) {
+                        // Top Left
+                        if (k > 0) {
+                            if (gridSystem[j-1][k-1] == 1) {
+                                neighbor++;
+                            }
+                        }
+                        // Top Right
+                        if (k != (width-1)) {
+                            if (gridSystem[j-1][k+1] == 1) {
+                                neighbor++;
+                            }
+                        }
+                        // Top
+                        if (gridSystem[j-1][k] == 1) {
+                            neighbor++;
+                        }
+                    }
+                    // Checks if the cell is NOT at the bottom row
+                    if (j != (height-1)) {
+                        // Bottom Left
+                        if (k > 0) {
+                            if (gridSystem[j+1][k-1] == 1) {
+                                neighbor++;
+                            }
+                        }
+                        // Bottom Right
+                        if (k != (width-1)) {
+                            if (gridSystem[j+1][k+1] == 1) {
+                                neighbor++;
+                            }
+                        }
+                        // Bottom
+                        if (gridSystem[j+1][k] == 1) {
+                            neighbor++;
+                        }
+                    }
+                    // Left
+                    if (k > 0) {
+                        if (gridSystem[j][k-1] == 1) {
+                            neighbor++;
+                        }
+                    }
+                    // Right
+                    if (k != (width-1)) {
+                        if (gridSystem[j][k+1] == 1) {
+                            neighbor++;
+                        }
+                    }
+                    neighborGrid[j][k] = neighbor;
+                }
+            }
+            for (j = 0; j < gridSystem.length; j++) {
+                for (k = 0; k < gridSystem[j].length; k++) {
+                    neighbor = neighborGrid[j][k];
+                    if (gridSystem[j][k] == 1) {
+                        if (neighbor < 2 || neighbor > 3) {
+                            gridSystem[j][k] = 0;
+                        }
+                    } else {
+                        if (neighbor == 3) {
+                            gridSystem[j][k] = 1;
+                        }
+                    }
+                }
+            }
+            drawBoard();
+        }, 100);
+    }
+}
+
+function patternSelection(pattern) {
+    switch (pattern) {
+        case 'point':
+            patternSelect = 0;
+            break;
+        case 'block':
+            patternSelect = 1;
+            break;
+        case 'glider':
+            patternSelect = 2;
+            break;
+        case 'blinker':
+            patternSelect = 3;
+            break;
+        case 'beacon':
+            patternSelect = 4;
+            break;
+        case 'toad':
+            patternSelect = 5;
+            break;
+        case 'pulsar':
+            patternSelect = 6;
+            break;
+        case 'spaceship':
+            patternSelect = 7;
+            break;
+        default:
+            patternSelect = 8;
+            break;
+    }
+}
+
+function toggleSection(sectionName) {
+    $('#'+sectionName).toggle('fast');
 }
